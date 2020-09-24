a379b6088a873aa79417e95aa5dc6b4e0d65f669
Holt-e <56269370+Holt-e@users.noreply.github.com>
2020-09-24 13:27:44 +0200

    Ferdig med 1B og fikset oppgave 2 litt

--- src/Oppgave1/OppgaveB.java
+++ src/Oppgave1/OppgaveB.java
@@ -6,14 +6,22 @@ public class OppgaveB {
 
     public static void main(String[] args){
 
-        BiFunction<Integer, Integer, Integer> add = (a, b)-> a + b;
-        BiFunction<Integer,Integer,Integer> biggest = (a, b)-> a > b ? a : b;
+        BiFunction<Integer, Integer, Integer> summerFunksjon = (a, b)-> a + b;
         int sum = beregn(12, 13, summerFunksjon);
+        System.out.println("Summen av 12 og 13 = " + sum);
+
+        BiFunction<Integer,Integer,Integer> stoersteVerdiFunksjon = (a, b)-> a > b ? a : b;
+        int stoersteVerdi = beregn(-5, 3, stoersteVerdiFunksjon);
+        System.out.println("Den største av -5 og 3 = " + stoersteVerdi);
+
+        BiFunction<Integer, Integer,Integer> avstandFunksjon = (a,b)-> a >= b ? a - b : b - a;
+        int avstand = beregn(54,45,avstandFunksjon);
+        System.out.println("Avstanden (absoluttverdien av differansen) mellom 54 og 45 = " + avstand);
     }
 
-    public static int beregn(int a, int b, summerFunksjon s){
+    public static int beregn(int a, int b, BiFunction<Integer,Integer,Integer> foo){
 
-      return s.sum(a,b);
+      return foo.apply(a,b);
     }
 
 }
