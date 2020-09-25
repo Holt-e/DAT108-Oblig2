79567eeb57ebab88f2165f55a87da944db1929bb
Holt-e <56269370+Holt-e@users.noreply.github.com>
2020-09-25 16:39:30 +0200

    Ferdig med Oppgave 3

--- src/Oppgave3/main.java
+++ src/Oppgave3/main.java
@@ -4,6 +4,7 @@ import java.awt.desktop.SystemSleepEvent;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class main {
 
@@ -25,7 +26,7 @@ public class main {
         List<String> etternavn = people.stream()
                 .map(Ansatt::getEtternavn)
                 .collect(Collectors.toList());
-        System.out.println(etternavn);
+        System.out.println(etternavn + "\n");
 
         //Oppgave 3b
         System.out.println("Oppgave 3b");
@@ -33,7 +34,7 @@ public class main {
         int antallKvinner = (int) people.stream()
                 .filter(p -> p.getKjonn().equals("Kvinne"))
                 .count();
-                System.out.println("Antall kvinner: " + antallKvinner);
+                System.out.println("Antall kvinner: " + antallKvinner + "\n");
 
         //Oppgave 3c
         System.out.println("Oppgave 3c");
@@ -41,7 +42,7 @@ public class main {
         int gjSnittsLonnKvinner = (Integer) people.stream()
                 .filter(p->p.getKjonn().equals("Kvinne"))
                 .mapToInt(p -> p.getAarslonn()).sum() / antallKvinner;
-                System.out.println("Gjennomsnittslonn for kvinner: " + gjSnittsLonnKvinner);
+                System.out.println("Gjennomsnittslonn for kvinner: " + gjSnittsLonnKvinner + "\n");
 
         //Oppgave 3d
         System.out.println("Oppgave 3d");
@@ -51,24 +52,43 @@ public class main {
                 .collect(Collectors.toList());
                 for(Ansatt s : sjefLonn)
                     s.setAarslonn((int) ((int) (s.getAarslonn()) * 1.07));
-                System.out.println("Ny lonn til alle Sjefer som okte med 7%: ");
+                System.out.println("Ny lonn til alle Sjefer som okte med 7%: " + "\n");
                 for (Ansatt s : people)
-                    System.out.println(s.toString());
+                    System.out.println(s.toString()+ "\n");
 
 
         //Oppgave 3e
         System.out.println("Oppgave 3e");
 
+        boolean tjenerMye = people.stream()
+                .filter(p -> p.getAarslonn() > 800000).count() > 0;
+
+        System.out.println("Er det noen som tjener mer en 800.000 kr: " + tjenerMye + "\n");
+
+
         //Oppgave 3f
-
         System.out.println("Oppgave 3f");
-        //Oppgave 3g
 
-        System.out.println("Oppgave 3g");
+        people.stream()
+        .forEach(System.out::println);
+        System.out.println();
+
+        //Oppgave 3g
+        System.out.println("Oppgave 3g ");
+
+        int lavestLonn = people.stream()
+                .mapToInt(Ansatt::getAarslonn).min().getAsInt();
+        List<Ansatt> minsteLonn = people.stream()
+                .filter(p->p.getAarslonn() == lavestLonn).collect(Collectors.toList());
+        System.out.println("Ansatt/Ansatte med minst lønn: " + minsteLonn + "\n");
 
         //Oppgave 3h
-
         System.out.println("Oppgave 3h");
+
+        int sum = IntStream.range(1,1000).filter(p -> p % 3 == 0|| p % 5 == 0).sum();
+        System.out.println(" Summen av alle heltall i [1, 1000> som er delelig med 3 eller 5.: " + sum);
+
+
 
 
         //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
